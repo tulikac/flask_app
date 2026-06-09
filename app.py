9833f8fee292288cd15066ca83573281e5fb8a6b
Tulika Chaudharie <tulikac@microsoft.com>
2026-06-09 20:48:12 +0530

    change

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! " + str(datetime.now())
+    return "Hello World! Making a change " + str(datetime.now())
 
 @app.route("/applicationpath")
 def applicationPath():
